f4617a335e1dc3823bcfe685b970a33729762f87
Heejeong Lim <ihj04982@gmail.com>
2025-08-06 23:05:37 +0900

    Add product retrieval functionality; implement getProducts endpoint in product controller and update API routing to include the new endpoint. Also, update .gitignore to exclude additional files.

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -21,4 +21,13 @@ productController.createProduct = async (req, res) => {
     }
 };
 
+productController.getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ status: "success", data: products });
+    } catch (error) {
+        res.status(400).json({ status: "error", message: error.message });
+    }
+};
+
 module.exports = productController;
--- routes/product.api.js
+++ routes/product.api.js
@@ -4,5 +4,6 @@ const productController = require("../controllers/product.controller");
 const authController = require("../controllers/auth.controller");
 
 router.post("/", authController.authenticate, authController.checkAdminPermission, productController.createProduct);
+router.get("/", productController.getProducts);
 
 module.exports = router;
